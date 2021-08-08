de5490b16507d9dd8076b230c5c69136bb70dc4a
ezrp <86675004+ezrp@users.noreply.github.com>
2021-08-08 20:27:15 +0200

    Update canvas.js

--- js/canvas.js
+++ js/canvas.js
@@ -8,7 +8,7 @@ function draw(){
     var cn = document.getElementById('canvas-about');
     if(cn.getContext){
         
-        if(i<1200){
+        if(i<1400){
             var ctx = cn.getContext('2d');
             var x = Math.random()*272;
             var y = Math.random()*140;
@@ -24,7 +24,27 @@ function draw(){
             ctx.fillText('E',x+12,y+13);
 
             i+=1; 
+
+            if (i==1400){
+
+                ctx.fillStyle = 'rgba(0,0,30,.8)'
+                ctx.fillRect(20,15,260,122);
+                ctx.fillStyle = 'rgb(250,300,300)';
+                ctx.font = 'normal 50px Segoe UI';
+                ctx.fillText('E',140,90);
+
+                
+            }
         }   
     window.requestAnimationFrame(draw);    
     } 
+}
+
+function colors(){
+    document.getElementById('about-paragraph').style.color = 'rgb(235, 235, 235)';
+    
+}
+
+function no_colors(){
+    document.getElementById('about-paragraph').style.color = 'rgba(207, 207, 207,.9)';
 }
