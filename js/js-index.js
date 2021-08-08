0e0fee3cd3ec46337b35ecfae9e6b62d801e6c84
ezrp <86675004+ezrp@users.noreply.github.com>
2021-08-08 20:25:42 +0200

    Update js-index.js

--- js/js-index.js
+++ js/js-index.js
@@ -13,16 +13,22 @@ $(document).ready(function(){
 });
 
 function mouseOver(){
-    document.getElementById('change-letter').className='changeLetter';
-    document.getElementById('canvas').style.borderColor='rgba(207, 207, 207,.9)';
+    document.getElementById('change-letter').style.color = '#B22222';
+    document.getElementById('main-title').style.backgroundColor = 'rgba(10,10,10,.8)';
+    document.getElementById('main-title').style.color = '#ffff'
+    document.getElementById('main-title').style.borderColor='rgba(207, 207, 207,.9)';
+
     document.getElementById('eye').style.color = 'rgb(0, 128, 128)';
     document.getElementById('book').style.color = 'rgb(233, 198, 0)';
     document.getElementById('fire').style.color = 'rgb(206, 7, 156)';
 }
 
 function mouseOut(){
-    document.getElementById('change-letter').className='unchangeLetter';
-    document.getElementById('canvas').style.borderColor='#555';
+    document.getElementById('change-letter').style.color='rgba(207, 207, 207,.9)';
+    document.getElementById('main-title').style.backgroundColor = '#050b16';
+    document.getElementById('main-title').style.color = 'rgba(207, 207, 207,.9)'
+    document.getElementById('main-title').style.borderColor='#555';
+
     document.getElementById('eye').style.color = 'rgba(207, 207, 207,.9)';
     document.getElementById('book').style.color = 'rgba(207, 207, 207,.9)';
     document.getElementById('fire').style.color = 'rgba(207, 207, 207,.9)';
